c5695aa0bb62e388a90f0b5afea44220e9f845da
R-kumarrrr <139208638+R-kumarrrr@users.noreply.github.com>
2024-05-08 10:37:13 +0000

    summary

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -10,7 +10,7 @@ console.log("Rajat"); // Code readability should be there.
 
 let name = "Rajat";
 let age = 20;
-let isLoggedIn = true;
+// let isLoggedIn = true;
 let state;
 
 // Number => 2 to power 52
@@ -25,3 +25,44 @@ let state;
 
 console.log(typeof undefined); undefined
 console.log(typeof null); Object
+
+// *********************** Summary ***********************
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
